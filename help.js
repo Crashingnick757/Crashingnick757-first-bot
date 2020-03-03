bb95bc36ac6aa81c5ba7c4b268bb8abc60c5380e
Crashingnick757 <53188670+Crashingnick757@users.noreply.github.com>
2020-03-03 13:58:52 -0500

    Update help.js

--- help.js
+++ help.js
@@ -4,6 +4,6 @@ const prefix = auth.prefix;
 
 module.exports = (message) => { // Function with 'message' parameter
 
-			message.channel.send("NOTE: This feature may not be complete yet.\nprefix: Shows the prefix.\ndevs: Shows a list of programmers.\nbalance: shows your balance\nping: Checks the bots ping\n").catch((e) => { console.log(e); });
+			message.channel.send("NOTE: This feature may not be complete yet.\nprefix: Shows the prefix.\ndevs: Shows a list of the bots devs.\nbalance/bal: shows your balance\nping: Checks the bots ping\ndice_roll <optional number of sides default is 6>: get a random number from 1 to 6 (or the specified number)\njoke: Responds with a random joke").catch((e) => { console.log(e); });
 
 }
